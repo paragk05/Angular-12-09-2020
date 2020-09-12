56b93b9249bccfe51b9a0f4acecaab1d881bef95
Paragk05-melayer <melayer.paragk@gmail.com>
2020-09-12 21:02:37 +0530

    app.js file modified

--- day1/app.js
+++ day1/app.js
@@ -1,28 +1,23 @@
 let list = [{ computer : 'HP', price : 35000, quantity : 2},
 { computer : 'Asus', price : 50000, quantity : 1},
-{ computer : 'Dell', price : 45000, quantity : 4}]
+{ computer : 'Dell', price : 45000, quantity : 4},
+{ computer : 'Lenovo', price : 40000, quantity : 3}]
 
 let tableId = document.getElementById('tableId')
 tableId.style.textAlign="center"
-
-for (let i = 0; i < list.length; i++) {
-    let comp = list[i].computer
-    let pr = list[i].price
-    let quan = list[i].quantity
-    let tot = list[i].price * list[i].quantity
-
+list.forEach(obj=> {
     let row = document.createElement('tr')
     let computer = document.createElement('td')
     let price = document.createElement('td')
     let quantity = document.createElement('td')
     let total = document.createElement('td')
-    computer.appendChild(document.createTextNode(comp))
-    price.appendChild(document.createTextNode(pr))
-    quantity.appendChild(document.createTextNode(quan))
-    total.appendChild(document.createTextNode((tot)))
+    computer.appendChild(document.createTextNode(obj.computer))
+    price.appendChild(document.createTextNode(obj.price))
+    quantity.appendChild(document.createTextNode(obj.quantity))
+    total.appendChild(document.createTextNode((obj.price * obj.quantity)))
     row.appendChild(computer)
     row.appendChild(price)
     row.appendChild(quantity)
     row.appendChild(total)
     tableId.appendChild(row)
-}
+})
